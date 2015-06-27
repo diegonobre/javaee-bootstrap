07f23066a2b1ecc719f01597cf9d0ef26daccb67
Diego Nobre <contato@diegonobre.net>
2015-06-27 17:53:43 -0300

    Create AssuntoDao.java

---
+++ src/com/github/diegonobre/javaee/main/jpa/AssuntoDao.java
@@ -0,0 +1,78 @@
+package br.com.linuxfi.dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import br.com.linuxfi.model.Assunto;
+
+public class AssuntoDao {
+
+	EntityManager em = Persistence.createEntityManagerFactory("jpacrud").createEntityManager();
+	
+	public void salvar(Assunto assunto) {
+		try {
+			
+			em.getTransaction().begin();
+			
+			em.merge(assunto);
+			
+			em.getTransaction().commit();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+		}
+	}
+	
+	public void remover(int id) {
+		try {
+			
+			em.getTransaction().begin();
+			
+			Assunto assunto = em.find(Assunto.class, id);
+			em.remove(assunto);
+			
+			em.getTransaction().commit();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+		}
+	}
+	
+	public List<Assunto> getTodos() {
+		List<Assunto> assuntos = new ArrayList<Assunto>();
+		
+		try {
+			
+			em.getTransaction().begin();
+			
+			Query query = em.createNamedQuery("Assunto.findAll");
+			
+			List<Object[]> list = query.getResultList();
+			Iterator iterator = list.iterator();
+			
+			while (iterator.hasNext()) {
+				Assunto assunto = (Assunto) iterator.next();
+				assuntos.add(assunto);
+			}
+			
+			em.getTransaction().commit();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			em.close();
+		}
+		
+		return assuntos;
+	}
+	
+}
